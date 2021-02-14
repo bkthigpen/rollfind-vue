6fca18605ee31b6be21b65bb1cf0c10149e7c887
Brandon Thigpen <brandonkthigpen@gmail.com>
2021-02-14 16:41:35 -0500

    Fetches detailed results data and tweaks app layout

---
+++ src/components/example.js
@@ -0,0 +1,54 @@
+<!-- <p><strong>name:</strong> {{ result.name }}</p>
+        <p><strong>text:</strong> {{ result.text }}</p> -->
+        <!-- <p v-if="result.highlighted">
+          <strong>highlighted:</strong> {{ result.highlighted }}
+        </p> -->
+        <!-- <p>{{ results.type }}</p> -->
+        <!-- <div v-for="(result, index) in searchResults.results" :key="index"> -->
+        <!-- <div v-if="result.document_slug">
+          <p>Document Slug: {{ result.document_slug }}</p>
+          <p>Name: {{ result.name }}</p>
+          <p>Text: {{ result.text }}</p>
+          <p>Route: {{ result.route }}</p>
+          <p>Slug: {{ result.slug }}</p>
+          <p>Type: {{ result.type }}</p>
+          <p>Rarity: {{ result.rarity }}</p>
+          <p v-if="result.attunement">
+            {{ result.attunement }}
+          </p>
+        </div>
+        <div v-else>
+          <p>Name: {{ result.name }}</p>
+          <p>Text: {{ result.text }}</p>
+          <p>Route: {{ result.route }}</p>
+          <p>Slug: {{ result.slug }}</p>
+          <p>armor_class: {{ result.armor_class }}</p>
+          <p>hit_points: {{ result.hit_points }}</p>
+          <p>hit_dice: {{ result.hit_dice }}</p>
+          <p>strength: {{ result.strength }}</p>
+          <p>dexterity: {{ result.dexterity }}</p>
+          <p>constitution: {{ result.constitution }}</p>
+          <p>strength: {{ result.intelligence }}</p>
+          <p>wisdom: {{ result.wisdom }}</p>
+          <p>charisma: {{ result.charisma }}</p>
+          <p>charisma: {{ charisma.charisma }}</p>
+          <p>challenge_rating: {{ charisma.challenge_rating }}</p>
+          <p>challenge_rating: {{ charisma.challenge_rating }}</p>
+        </div> -->
+        <pre>{{ result }}</pre>
+        <!-- <div class="Results__content-card">
+            <p>Name: {{ result.name }}</p>
+            <p>Hit Points: {{ result.hit_points }}</p>
+            <p>Hit Dice: {{ result.hit_dice }}</p>
+            <p>Strength: {{ result.strength }}</p>
+            <p>Dexterity: {{ result.dexterity }}</p>
+            <p>Constitution: {{ result.constitution }}</p>
+            <p>Intelligence: {{ result.intelligence }}</p>
+            <p>Wisom: {{ result.wisdom }}</p>
+            <p>Charisma: {{ result.charisma }}</p>
+            <p>Challenge Rating: {{ result.challenge_rating }}</p>
+          </div> -->
+        <!-- <div v-else>
+          Other Results
+        </div> -->
+        <!-- </div> -->
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,36 +1,42 @@
 <template>
-  <div class="Index px-4 pt-72 pb-4 text-center">
-    <h1 class="my-2">
-      <span class="text-blue-100">Roll</span>
-      <span class="text-yellow-400 mr-2">Find</span>
-      <img class="Index__vue-logo" src="../assets/images/vue.png" />
-    </h1>
-    <p class="w-full text-2xl">
-      Quick search of ⚔️ D&D rules, stats, details, and general info.
-    </p>
-    <div class="mt-4 flex">
-      <TextInput v-model="search" />
-      <Button @click="getSearchResults" />
-    </div>
-    <div class="Results mt-4">
-      <p>{{ searchResults.count }} found</p>
-      <div class="Results__content my-4">
+  <div class="Index grid md:grid-cols-2">
+    <form class="Index__form transform md:translate-x-1/2 py-96">
+      <h1 class="my-2 text-center">
+        <span class="text-blue-100">Roll</span>
+        <span class="text-yellow-400 mr-2">Find</span>
+        <img class="Index__vue-logo" src="../assets/images/vue.png" />
+      </h1>
+      <p class="w-full text-2xl text-center">
+        Quick search of ⚔️ D&D rules, stats, details, and general info.
+      </p>
+      <div class="mt-4 flex">
+        <TextInput v-model="search" />
+        <Button @click="getSearchResults" />
+      </div>
+      <div class="Results mt-4">
+        <p>{{ searchResults.count }} found</p>
         <div
           v-for="(result, index) in searchResults.results"
           :key="index"
-          class="Results__content-card"
+          class="Results__content my-4"
         >
-          <p>Name: {{ result.name }}</p>
-          <p>Hit Points: {{ result.hit_points }}</p>
-          <p>Hit Dice: {{ result.hit_dice }}</p>
-          <p>Strength: {{ result.strength }}</p>
-          <p>Dexterity: {{ result.dexterity }}</p>
-          <p>Constitution: {{ result.constitution }}</p>
-          <p>Intelligence: {{ result.intelligence }}</p>
-          <p>Wisom: {{ result.wisdom }}</p>
-          <p>Charisma: {{ result.charisma }}</p>
-          <p>Challenge Rating: {{ result.challenge_rating }}</p>
+          <div @click="getDetailedResults(result.route, result.slug)">
+            <p>
+              <strong>{{ result.name }}</strong>
+            </p>
+            <p class="truncate ..."><strong>Text</strong>: {{ result.text }}</p>
+            <p class="capitalize">
+              <strong>Route</strong>: {{ formatRoute(result.route) }}
+            </p>
+            <br />
+            {{ result }}
+          </div>
         </div>
+      </div>
+    </form>
+    <div v-if="detailedResults" class="DetailedResults">
+      <div class="px-4 pt-10">
+        {{ detailedResults }}
       </div>
     </div>
   </div>
@@ -51,14 +57,40 @@ export default {
   setup() {
     const search = ref("");
     const searchResults = ref("");
+    const detailedResults = ref("");
 
-    function getSearchResults() {
+    const getSearchResults = () => {
       axios
         .get(`https://api.open5e.com/search/?text=${search.value}`)
         .then(response => (searchResults.value = response.data));
-    }
+    };
 
-    return { getSearchResults, search, searchResults };
+    const getDetailedResults = (route, slug) => {
+      axios
+        .get(`https://api.open5e.com/${route}${slug}`)
+        .then(response => (detailedResults.value = response.data));
+    };
+
+    const formatRoute = url => {
+      let newUrl = url;
+      newUrl = newUrl.replace("/", "");
+
+      // maybe rework this?
+      if (newUrl === "magicitems") {
+        newUrl = newUrl.split("c").join("c ");
+      }
+
+      return newUrl;
+    };
+
+    return {
+      detailedResults,
+      formatRoute,
+      getDetailedResults,
+      getSearchResults,
+      search,
+      searchResults
+    };
   }
 };
 </script>
